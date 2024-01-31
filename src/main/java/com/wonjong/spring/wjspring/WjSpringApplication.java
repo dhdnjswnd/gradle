ce4c844e0195c20954694d7fe80b468126b60021
dhdnjswnd <dhdnjswnd@gmail.com>
2024-01-31 21:36:32 +0900

    first commit

---
+++ src/main/java/com/wonjong/spring/wjspring/WjSpringApplication.java
@@ -0,0 +1,13 @@
+package com.wonjong.spring.wjspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WjSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WjSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wonjong/spring/wjspring/WjSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonjong.spring.wjspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WjSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
